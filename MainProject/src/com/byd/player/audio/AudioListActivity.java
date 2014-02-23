158735ec7d9343a004619a9d4adb33331743a74d
timlian <timlian@yeah.net>
2014-02-23 22:19:16 +0800

    Add audio list header

--- MainProject/src/com/byd/player/audio/AudioListActivity.java
+++ MainProject/src/com/byd/player/audio/AudioListActivity.java
@@ -1,12 +1,7 @@
 package com.byd.player.audio;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -14,14 +9,14 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 
+import com.byd.player.BaseActivity;
 import com.byd.player.R;
 
-public class AudioListActivity extends Activity implements OnItemClickListener {
+public class AudioListActivity extends BaseActivity implements OnItemClickListener {
     private GridView mAudioList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -34,24 +29,6 @@ public class AudioListActivity extends Activity implements OnItemClickListener {
         BaseAdapter adapter = new AudioAdapter(this, getLayoutInflater());
         mAudioList.setAdapter(adapter);
         mAudioList.setOnItemClickListener(this);
-
-        ActionBar actionBar = this.getActionBar();
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_audio_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_edit:
-            break;
-        }
-        return true;
     }
 
     @Override
