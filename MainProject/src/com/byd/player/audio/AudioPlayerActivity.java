73c1a074e3bcafe44af856276dc7e6d3634acce1
timlian <timlian@yeah.net>
2014-02-25 00:39:14 +0800

    Make the audio player UI according to the design

--- MainProject/src/com/byd/player/audio/AudioPlayerActivity.java
+++ MainProject/src/com/byd/player/audio/AudioPlayerActivity.java
@@ -1,46 +1,19 @@
 package com.byd.player.audio;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.byd.player.BaseActivity;
 import com.byd.player.R;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-
-public class AudioPlayerActivity extends Activity {
+public class AudioPlayerActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.audio_player_view);
-        ActionBar actionBar = this.getActionBar();
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_audio_player, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-        case R.id.action_more:
-            finish();
-            break;
-        }
-        return true;
     }
 }
