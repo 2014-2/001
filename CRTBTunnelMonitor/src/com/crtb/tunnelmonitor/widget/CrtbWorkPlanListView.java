4b0535b2ecb86b65ab0bb49c0ee678c830ba5c21
chengduzhouwei <chengdu.zhou.wei@gmail.com>
2014-04-10 00:45:51 +0800

    屏蔽代码

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/widget/CrtbWorkPlanListView.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/widget/CrtbWorkPlanListView.java
@@ -40,15 +40,15 @@ public final class CrtbWorkPlanListView extends CrtbBaseListView {
 	@Override
 	public void onResume() {
 		
-		WorkPlan wp = new WorkPlan() ;
-		wp.setWorkPlanName("成都红星路隧道1#");
-		wp.setMileagePrefix("CDHX");
-		wp.setCreationTime(DateUtils.toDateString(DateUtils.getCurrtentTimes()));
-		wp.setStartMileage(0.0f);
-		wp.setEndMileage(100.0f);
-		wp.setConstructionOrganization("成都路桥建设工程有限公司");
-		
-		WorkPlanDao.defaultWorkPlanDao().insert(wp);
+//		WorkPlan wp = new WorkPlan() ;
+//		wp.setWorkPlanName("成都红星路隧道1#");
+//		wp.setMileagePrefix("CDHX");
+//		wp.setCreationTime(DateUtils.toDateString(DateUtils.getCurrtentTimes()));
+//		wp.setStartMileage(0.0f);
+//		wp.setEndMileage(100.0f);
+//		wp.setConstructionOrganization("成都路桥建设工程有限公司");
+//		
+//		WorkPlanDao.defaultWorkPlanDao().insert(wp);
 		
 		if(mAdapter.isEmpty()){
 			onReload();
