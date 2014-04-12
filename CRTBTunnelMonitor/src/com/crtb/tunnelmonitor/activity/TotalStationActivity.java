988977596a80f05083ca60ff883e92943da6ce41
edisonxiao007 <xiang.xiao.911@gmail.com>
2014-04-12 16:59:34 +0800

    modify station

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/TotalStationActivity.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/TotalStationActivity.java
@@ -1,44 +1,43 @@
 package com.crtb.tunnelmonitor.activity;
 
-import com.crtb.tunnelmonitor.activity.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class TotalStationActivity extends Activity {
 	/**
 	 * 全站仪连接管理
 	 */
-	private RelativeLayout total;
+	private LinearLayout mStation;
 	/**
 	 * 控点管理
 	 */
-	private RelativeLayout conter;
+	private LinearLayout mControlPoint;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_totalstation);
 		init();
 	}
 	private void init() {
-		total=(RelativeLayout) findViewById(R.id.total_qzy);
-		total.setOnClickListener(listener);
-		conter=(RelativeLayout) findViewById(R.id.total_kd);
-		conter.setOnClickListener(listener);
+		mStation=(LinearLayout) findViewById(R.id.station);
+		mStation.setOnClickListener(listener);
+		mControlPoint=(LinearLayout) findViewById(R.id.control_point);
+		mControlPoint.setOnClickListener(listener);
 	}
 	private OnClickListener listener = new OnClickListener() {
 		
 		public void onClick(View v) {
 			Intent intent;
 			switch(v.getId()){
-				case R.id.total_qzy://全站仪连接管理
+				case R.id.station://全站仪连接管理
 					intent = new Intent(TotalStationActivity.this,StationActivity.class);
 					startActivity(intent);
 					break;
-				case R.id.total_kd://控点管理管理
+				case R.id.control_point://控点管理管理
 					intent = new Intent(TotalStationActivity.this,ControlPointsActivity.class);
 					startActivity(intent);
 					break;
