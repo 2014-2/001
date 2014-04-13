1f3d41d7a33a846ab39a20586da0db937ca335fc
chengduzhouwei <chengdu.zhou.wei@gmail.com>
2014-04-14 00:53:47 +0800

    添加执行测量

---
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/TestSectionExecuteActivity.java
@@ -0,0 +1,21 @@
+package com.crtb.tunnelmonitor.activity;
+
+import android.os.Bundle;
+
+import com.crtb.tunnelmonitor.WorkFlowActivity;
+
+/**
+ * 开始测量
+ * 
+ * @author zhouwei
+ *
+ */
+public class TestSectionExecuteActivity extends WorkFlowActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+}
