8b900e5a1cae36b237b6132f48f2042215e51f72
U-CHINA\Administrator <Administrator@QH-20140330GABX.(none)>
2014-04-13 21:20:17 +0800

    Tuning register UI

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/RegisterActivity.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/RegisterActivity.java
@@ -1,11 +1,15 @@
 package com.crtb.tunnelmonitor.activity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends Activity implements OnClickListener{
 
@@ -31,6 +35,7 @@ public class RegisterActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		switch(v.getId()){
 		case R.id.ok:
+		    showDialog(false, null);
 			break;
 		case R.id.cancel:
 			RegisterActivity.this.finish();
@@ -38,5 +43,31 @@ public class RegisterActivity extends Activity implements OnClickListener{
 		}	
 	}
 
-
+	private void showDialog(boolean bSuccess,final OnClickListener listener){
+        final Dialog dlg=new Dialog(this,R.style.custom_dlg);
+        View view=LayoutInflater.from(this).inflate(R.layout.success_dialog_layout, null);      
+        dlg.setContentView(view);
+        TextView text=(TextView) dlg.findViewById(R.id.text);
+        if(!bSuccess){
+            text.setText("注册失败！ 请确定输入的信息正确！");
+            text.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_warnning,0, 0, 0);
+        }
+        Button bt=(Button) dlg.findViewById(R.id.bt);
+        bt.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+               if(listener!=null){
+                   listener.onClick(v);
+               }
+               if(dlg!=null){
+                   dlg.dismiss();
+               }
+            }
+        });
+        dlg.show();
+        WindowManager.LayoutParams param=dlg.getWindow().getAttributes();
+        param.width=getWindowManager().getDefaultDisplay().getWidth()*3/4;
+        dlg.getWindow().setAttributes(param);
+    }
 }
