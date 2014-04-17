89812d8f8c5469d41247425299da1a0f23d7ab42
timlian <timlian@yeah.net>
2014-04-17 17:01:31 +0800

    Add menu in warning upload

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/WarningUploadActivity.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/WarningUploadActivity.java
@@ -1,17 +1,90 @@
 package com.crtb.tunnelmonitor.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class WarningUploadActivity extends Activity {
+    private MenuPopupWindow menuWindow;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_warning_upload);
-		TextView title=(TextView) findViewById(R.id.tv_topbar_title);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warning_upload);
+        TextView title=(TextView) findViewById(R.id.tv_topbar_title);
         title.setText(R.string.upload_warning_data);
-	}
+    }
 
+    class MenuPopupWindow extends PopupWindow {
+        public RelativeLayout chakan;
+        public RelativeLayout shangchuan;
+        private View mMenuView;
+        private Intent intent;
+        public Context c;
+        AlertDialog dlg = null;
+
+        public MenuPopupWindow(Activity context) {
+            super(context);
+            this.c = context;
+            dlg = new AlertDialog.Builder(c).create();
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mMenuView = inflater.inflate(R.layout.menu_warning_upload, null);
+            chakan = (RelativeLayout) mMenuView.findViewById(R.id.menu_ck);
+            shangchuan = (RelativeLayout) mMenuView.findViewById(R.id.menu_sc);
+
+            chakan.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO：
+                }
+            });
+            shangchuan.setOnClickListener(new OnClickListener() {
+
+                // @SuppressLint("ShowToast")
+                @Override
+                public void onClick(View v) {
+                    //TODO:
+                }
+            });
+            setContentView(mMenuView);
+            setWidth(LayoutParams.FILL_PARENT);
+            setHeight(LayoutParams.WRAP_CONTENT);
+            // 设置SelectPicPopupWindow弹出窗体可点击
+            setFocusable(true);
+            // 实例化一个ColorDrawable颜色为半透明
+            ColorDrawable dw = new ColorDrawable(0xFF000000);
+            // 设置SelectPicPopupWindow弹出窗体的背景
+            setBackgroundDrawable(dw);
+            setOutsideTouchable(true);
+        };
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_MENU) {
+                if (menuWindow == null) {
+                    menuWindow = new MenuPopupWindow(this);
+                }
+                menuWindow.showAtLocation(new View(this), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+            }
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                this.finish();
+            }
+        }
+        return true;
+    }
 }
