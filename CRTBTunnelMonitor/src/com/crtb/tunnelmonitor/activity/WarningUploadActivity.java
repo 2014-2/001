b38a017add3b162c6d4cf6f1a94ba23543d2f962
timlian <timlian@yeah.net>
2014-04-27 17:14:46 +0800

    Modify upload progress bar ui.

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/WarningUploadActivity.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/WarningUploadActivity.java
@@ -16,19 +16,22 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.crtb.tunnelmonitor.network.CrtbWebService;
 import com.crtb.tunnelmonitor.network.RpcCallback;
@@ -40,6 +43,11 @@ public class WarningUploadActivity extends Activity {
     private static final String LOG_TAG = "WarningUploadActivity";
     private MenuPopupWindow menuWindow;
     private ListView mlvWarningList;
+    private LinearLayout mProgressOverlay;
+    private ProgressBar mUploadProgress;
+    private ImageView mUploadStatusIcon;
+    private TextView mUploadStatusText;
+    private boolean isUploading = true;
     private WarningUploadAdapter mAdapter;
 
     private Random ran = new Random();
@@ -58,38 +66,80 @@ public class WarningUploadActivity extends Activity {
         title.setText(R.string.upload_warning_data);
         initB();
         init();
+        initProgressOverlay();
     }
 
     private void init() {
         mlvWarningList = (ListView) findViewById(R.id.warning_list);
         mlvWarningList.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				mAdapter.revertCheck(position);
-			}
-		});
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mAdapter.revertCheck(position);
+            }
+        });
         mAdapter = new WarningUploadAdapter();
         mlvWarningList.setAdapter(mAdapter);
         loadData();
     }
 
-    private void loadData() {
-    	WarningDataManager dataManager = new WarningDataManager();
-        dataManager.loadData(new WarningLoadListener() {
-			@Override
-			public void done(List<UploadWarningData> uploadDataList) {
-				List<WarningUploadData> warningDataList = new ArrayList<WarningUploadData>();
-				for(UploadWarningData uploadWarningData : uploadDataList) {
-					WarningUploadData warningData = new WarningUploadData();
-					warningData.setUploadWarningData(uploadWarningData);
-					warningData.setChecked(false);
-					warningDataList.add(warningData);
-				}
-				mAdapter.setWarningData(warningDataList);
-			}
-		});
+    private void initProgressOverlay() {
+        mProgressOverlay = (LinearLayout)findViewById(R.id.progress_overlay);
+        mUploadProgress = (ProgressBar)findViewById(R.id.progressbar);
+        mUploadStatusIcon = (ImageView)findViewById(R.id.upload_status_icon);
+        mUploadStatusText = (TextView)findViewById(R.id.upload_status_text);
+        mProgressOverlay.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (!isUploading) {
+                    hideProgressOverlay();
+                }
+                return true;
+            }
+        });
     }
-    
+
+    private void showProgressOverlay() {
+        mProgressOverlay.setVisibility(View.VISIBLE);
+        mUploadProgress.setIndeterminate(true);
+        mUploadStatusIcon.setVisibility(View.GONE);
+        mUploadStatusText.setText(R.string.data_uploading_alert);
+        isUploading = true;
+    }
+
+    private void hideProgressOverlay() {
+        mProgressOverlay.setVisibility(View.GONE);
+    }
+
+    private void updateStatus(boolean isSuccess) {
+        isUploading = false;
+        mUploadStatusIcon.setVisibility(View.VISIBLE);
+        if (isSuccess) {
+            mUploadStatusIcon.setImageResource(R.drawable.success);
+            mUploadStatusText.setText(R.string.data_upload_alert_success);
+        } else {
+            mUploadStatusIcon.setImageResource(R.drawable.fail);
+            mUploadStatusText.setText(R.string.data_upload_alert_fail);
+        }
+    }
+
+    private void loadData() {
+        WarningDataManager dataManager = new WarningDataManager();
+        dataManager.loadData(new WarningLoadListener() {
+            @Override
+            public void done(List<UploadWarningData> uploadDataList) {
+                List<WarningUploadData> warningDataList = new ArrayList<WarningUploadData>();
+                for(UploadWarningData uploadWarningData : uploadDataList) {
+                    WarningUploadData warningData = new WarningUploadData();
+                    warningData.setUploadWarningData(uploadWarningData);
+                    warningData.setChecked(false);
+                    warningDataList.add(warningData);
+                }
+                mAdapter.setWarningData(warningDataList);
+            }
+        });
+    }
+
     class MenuPopupWindow extends PopupWindow {
         public RelativeLayout chakan;
         public RelativeLayout shangchuan;
@@ -112,6 +162,7 @@ public class WarningUploadActivity extends Activity {
                 @Override
                 public void onClick(View v) {
                     //TODO：
+                    menuWindow.dismiss();
                 }
             });
             shangchuan.setOnClickListener(new OnClickListener() {
@@ -119,6 +170,7 @@ public class WarningUploadActivity extends Activity {
                 @Override
                 public void onClick(View v) {
                     uploadWarningData();
+                    menuWindow.dismiss();
                 }
             });
             setContentView(mMenuView);
@@ -151,18 +203,23 @@ public class WarningUploadActivity extends Activity {
     }
 
     private void uploadWarningData() {
+        showProgressOverlay();
         CrtbWebService.getInstance().uploadWarningData(null, new RpcCallback() {
 
             @Override
             public void onSuccess(Object[] data) {
                 Log.d(LOG_TAG, "upload warning data success.");
-				Toast.makeText(getApplicationContext(), "上传预警信息成功", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), "上传预警信息成功",
+                // Toast.LENGTH_SHORT).show();
+                updateStatus(true);
             }
 
             @Override
             public void onFailed(String reason) {
                 Log.d(LOG_TAG, "upload warning data failed.");
-				Toast.makeText(getApplicationContext(), "上传预警信息失败", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), "上传预警信息失败",
+                // Toast.LENGTH_SHORT).show();
+                updateStatus(false);
             }
         });
     }
@@ -182,32 +239,32 @@ public class WarningUploadActivity extends Activity {
     }
 
     private static class ViewHolder {
-    	TextView mWarningTime;
-    	TextView mWarningState;
-    	TextView mWarningUpload;
-    	ImageView mWarningCheck;
+        TextView mWarningTime;
+        TextView mWarningState;
+        TextView mWarningUpload;
+        ImageView mWarningCheck;
     }
-    
+
     private class WarningUploadAdapter extends BaseAdapter {
-    	private List<WarningUploadData> mWarningDataList;
-    	
-    	WarningUploadAdapter() {
-    		mWarningDataList = new ArrayList<WarningUploadData>();
-    	}
-    	
-    	public void setWarningData(List<WarningUploadData> warningDataList) {
-    		if (warningDataList != null) {
-    			mWarningDataList = warningDataList;
-    			notifyDataSetChanged();
-    		}
-    	}
-    	
-    	public void revertCheck(int position) {
-    		WarningUploadData warningUploadData = mWarningDataList.get(position);
-    		warningUploadData.setChecked(!warningUploadData.isChecked());
+        private List<WarningUploadData> mWarningDataList;
+
+        WarningUploadAdapter() {
+            mWarningDataList = new ArrayList<WarningUploadData>();
+        }
+
+        public void setWarningData(List<WarningUploadData> warningDataList) {
+            if (warningDataList != null) {
+                mWarningDataList = warningDataList;
+                notifyDataSetChanged();
+            }
+        }
+
+        public void revertCheck(int position) {
+            WarningUploadData warningUploadData = mWarningDataList.get(position);
+            warningUploadData.setChecked(!warningUploadData.isChecked());
             notifyDataSetChanged();
         }
-    	
+
         @Override
         public int getCount() {
             return mWarningDataList.size();
@@ -237,41 +294,41 @@ public class WarningUploadActivity extends Activity {
             bindView(mWarningDataList.get(position), convertView);
             return convertView;
         }
-        
+
         private void bindView(WarningUploadData warningData, View convertView) {
-        	ViewHolder holder = (ViewHolder)convertView.getTag();
-        	holder.mWarningTime.setText(warningData.getUploadWarningData().getAlertInfo().getDate());
-        	holder.mWarningState.setText(warningData.getUploadWarningData().getAlertInfo().getAlertStatusMsg());
-        	if (warningData.isUploaded()) {
-        		holder.mWarningUpload.setText("已上传");
-        	} else {
-        		holder.mWarningUpload.setText("未上传");
-        	}
-        	if (warningData.isChecked()) {
-        		holder.mWarningCheck.setImageResource(R.drawable.yes);
-        	} else {
-        		holder.mWarningCheck.setImageResource(R.drawable.no);
-        	}
+            ViewHolder holder = (ViewHolder)convertView.getTag();
+            holder.mWarningTime.setText(warningData.getUploadWarningData().getAlertInfo().getDate());
+            holder.mWarningState.setText(warningData.getUploadWarningData().getAlertInfo().getAlertStatusMsg());
+            if (warningData.isUploaded()) {
+                holder.mWarningUpload.setText("已上传");
+            } else {
+                holder.mWarningUpload.setText("未上传");
+            }
+            if (warningData.isChecked()) {
+                holder.mWarningCheck.setImageResource(R.drawable.yes);
+            } else {
+                holder.mWarningCheck.setImageResource(R.drawable.no);
+            }
         }
     }
-    
+
     private class WarningUploadData {
-    	private UploadWarningData mUploadWarningData;
-    	private boolean mIsChecked;
-    	
-    	public void setUploadWarningData(UploadWarningData uploadWarningData) {
-    		mUploadWarningData = uploadWarningData;
-    	}
-    	
-    	public UploadWarningData getUploadWarningData() {
-    		return mUploadWarningData;
-    	}
-    	
-    	public boolean isUploaded() {
-    		return false;
-    	}
-    	
-    	public void setChecked(boolean checked) {
+        private UploadWarningData mUploadWarningData;
+        private boolean mIsChecked;
+
+        public void setUploadWarningData(UploadWarningData uploadWarningData) {
+            mUploadWarningData = uploadWarningData;
+        }
+
+        public UploadWarningData getUploadWarningData() {
+            return mUploadWarningData;
+        }
+
+        public boolean isUploaded() {
+            return false;
+        }
+
+        public void setChecked(boolean checked) {
             mIsChecked = checked;
         }
 
