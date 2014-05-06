67ccd23c8f990c4572095247e3a9614be4dcd73c
timlian <timlian@yeah.net>
2014-05-06 21:34:11 +0800

    Show progress dialog when loading tester.

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/TesterLoadActivity.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/activity/TesterLoadActivity.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -29,174 +30,185 @@ import com.crtb.tunnelmonitor.utils.CrtbAppConfig;
 
 public class TesterLoadActivity extends Activity implements OnClickListener {
 
-	private Button mLoad;
+    private Button mLoad;
 
-	private EditText mServerAddress;
-	
-	private EditText mUserName;
+    private EditText mServerAddress;
 
-	private EditText mPassword;
+    private EditText mUserName;
 
-	private RelativeLayout mTvLayout;
+    private EditText mPassword;
 
-	private List<SurveyerInformation> mTestors ;
+    private RelativeLayout mTvLayout;
 
-	private ListView mListView;
+    private List<SurveyerInformation> mTestors ;
 
-	private TestorAdapter mAdapter;
+    private ListView mListView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.download_tester_layout);
-		mServerAddress = (EditText) findViewById(R.id.server_ip);
-		mServerAddress.setText("http://61.237.239.144/fxkz/basedown");
-		mServerAddress.setSingleLine(true);
-		mLoad = (Button) findViewById(R.id.load);
-		mUserName = (EditText) findViewById(R.id.username);
-		mPassword = (EditText) findViewById(R.id.password);
-		mTvLayout = (RelativeLayout) findViewById(R.id.tv_layout);
+    private TestorAdapter mAdapter;
 
-		mLoad.setOnClickListener(this);
-		mListView = (ListView) findViewById(R.id.listview);
-		init();
-	}
+    private ProgressDialog mProgressDialog;
 
-	private void init() {
-		
-		mTestors = SurveyerInformationDao.defaultDao().queryAllSurveyerInformation() ;
-		
-		if (mTestors!=null && mTestors.size() > 0) {
-			mTvLayout.setVisibility(View.VISIBLE);
-			mAdapter = new TestorAdapter();
-			mListView.setAdapter(mAdapter);
-		}
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.download_tester_layout);
+        mServerAddress = (EditText) findViewById(R.id.server_ip);
+        mServerAddress.setText("http://61.237.239.144/fxkz/basedown");
+        mServerAddress.setSingleLine(true);
+        mLoad = (Button) findViewById(R.id.load);
+        mUserName = (EditText) findViewById(R.id.username);
+        mPassword = (EditText) findViewById(R.id.password);
+        mTvLayout = (RelativeLayout) findViewById(R.id.tv_layout);
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.load:
-			
-			String name = mUserName.getText().toString().trim();
-			String pwd = mPassword.getText().toString().trim();
-			
-			if (TextUtils.isEmpty(name) || TextUtils.isEmpty(pwd)) {
-				Toast.makeText(this, "请填写用户名和密码", Toast.LENGTH_LONG).show();
-				return;
-			}
-			
-			login(name, pwd);
-			
-			break;
-		}
-	}
+        mLoad.setOnClickListener(this);
+        mListView = (ListView) findViewById(R.id.listview);
+        init();
+    }
 
-	private void login(final String username, final String password) {
-		
-		CrtbWebService.getInstance().login(username, password,
-				new RpcCallback() {
+    private void init() {
 
-					@Override
-					public void onSuccess(Object[] data) {
-						// Toast.makeText(TesterLoadActivity.this,
-						// "获取成功",Toast.LENGTH_LONG).show();
-						CrtbAppConfig config = CrtbAppConfig.getInstance();
-						config.setUserName(username);
-						config.setPassword(password);
-						CrtbWebService.getInstance().getSurveyors(
-								new RpcCallback() {
+        mTestors = SurveyerInformationDao.defaultDao().queryAllSurveyerInformation() ;
 
-									@Override
-									public void onSuccess(Object[] data) {
-										
-										if (data != null && data.length > 0) {
-											
-											mTestors = Arrays
-													.asList((SurveyerInformation[]) data);
-											
-											SurveyerInformationDao.defaultDao().deleteAll() ;
-											
-											mTvLayout.setVisibility(View.VISIBLE);
-                                            mAdapter=new TestorAdapter();
-											mListView.setAdapter(mAdapter);
-											
-                                            for (int i = 0; i < mTestors.size(); i++){
-                                            	SurveyerInformationDao.defaultDao().insert(mTestors.get(i)) ;
-											}
-                                            
-                                            // 通知
-                                            sendBroadcast(new Intent(AppConfig.ACTION_RELOAD_ALL_SURVEYER));
-                                            
-                                            EventDispatcher.getInstance().notifyDatabaseChanged();
-										}
-									}
+        if (mTestors!=null && mTestors.size() > 0) {
+            mTvLayout.setVisibility(View.VISIBLE);
+            mAdapter = new TestorAdapter();
+            mListView.setAdapter(mAdapter);
+        }
+    }
 
-									@Override
-									public void onFailed(String reason) {
-										
-									}
-								});
-					}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.load:
 
-					@Override
-					public void onFailed(String reason) {
-						// Toast.makeText(TesterLoadActivity.this, "用户名或密码错误",
-						// Toast.LENGTH_LONG).show();
+                String name = mUserName.getText().toString().trim();
+                String pwd = mPassword.getText().toString().trim();
 
-					}
-				});
-	}
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(pwd)) {
+                    Toast.makeText(this, "请填写用户名和密码", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (mProgressDialog == null) {
+                    mProgressDialog = ProgressDialog.show(TesterLoadActivity.this, null,
+                            getString(R.string.tester_loading), true, false);
+                }
+                mProgressDialog.show();
+                login(name, pwd);
 
-	class TestorAdapter extends BaseAdapter {
+                break;
+        }
+    }
 
-		@Override
-		public int getCount() {
-			if (mTestors != null) {
-				return mTestors.size();
-			}
-			return 0;
-		}
+    private void login(final String username, final String password) {
 
-		@Override
-		public Object getItem(int pos) {
-			if (mTestors != null) {
-				return mTestors.get(pos);
-			}
-			return null;
-		}
+        CrtbWebService.getInstance().login(username, password,
+                new RpcCallback() {
 
-		@Override
-		public long getItemId(int id) {
-			return id;
-		}
+            @Override
+            public void onSuccess(Object[] data) {
+                // Toast.makeText(TesterLoadActivity.this,
+                // "获取成功",Toast.LENGTH_LONG).show();
+                CrtbAppConfig config = CrtbAppConfig.getInstance();
+                config.setUserName(username);
+                config.setPassword(password);
+                CrtbWebService.getInstance().getSurveyors(
+                        new RpcCallback() {
 
-		@Override
-		public View getView(int pos, View view, ViewGroup arg2) {
-			ViewHolder holder = null;
-			if (view == null) {
-				view = LayoutInflater.from(TesterLoadActivity.this).inflate(
-						R.layout.testor_item_layout, null);
-				holder = new ViewHolder();
-				holder.name = (TextView) view.findViewById(R.id.name);
-				holder.idCard = (TextView) view.findViewById(R.id.idcard);
-				holder.note = (TextView) view.findViewById(R.id.note);
-				view.setTag(holder);
-			} else {
-				holder = (ViewHolder) view.getTag();
-			}
-			SurveyerInformation info = mTestors.get(pos);
-			holder.name.setText(info.getSurveyerName());
-			holder.idCard.setText(info.getCertificateID());
-			holder.note.setText(info.getInfo());
-			return view;
-		}
+                            @Override
+                            public void onSuccess(Object[] data) {
 
-	}
+                                if (data != null && data.length > 0) {
 
-	class ViewHolder {
-		TextView name;
-		TextView idCard;
-		TextView note;
-	}
+                                    mTestors = Arrays
+                                            .asList((SurveyerInformation[]) data);
+
+                                    SurveyerInformationDao.defaultDao().deleteAll() ;
+
+                                    mTvLayout.setVisibility(View.VISIBLE);
+                                    mAdapter=new TestorAdapter();
+                                    mListView.setAdapter(mAdapter);
+
+                                    for (int i = 0; i < mTestors.size(); i++){
+                                        SurveyerInformationDao.defaultDao().insert(mTestors.get(i)) ;
+                                    }
+
+                                    // 通知
+                                    sendBroadcast(new Intent(AppConfig.ACTION_RELOAD_ALL_SURVEYER));
+
+                                    EventDispatcher.getInstance().notifyDatabaseChanged();
+                                    mProgressDialog.dismiss();
+                            Toast.makeText(TesterLoadActivity.this, R.string.tester_load_success,
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailed(String reason) {
+                                mProgressDialog.dismiss();
+                        Toast.makeText(TesterLoadActivity.this, R.string.tester_load_failed,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+
+            @Override
+            public void onFailed(String reason) {
+                mProgressDialog.dismiss();
+                Toast.makeText(TesterLoadActivity.this, R.string.tester_load_failed,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    class TestorAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            if (mTestors != null) {
+                return mTestors.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            if (mTestors != null) {
+                return mTestors.get(pos);
+            }
+            return null;
+        }
+
+        @Override
+        public long getItemId(int id) {
+            return id;
+        }
+
+        @Override
+        public View getView(int pos, View view, ViewGroup arg2) {
+            ViewHolder holder = null;
+            if (view == null) {
+                view = LayoutInflater.from(TesterLoadActivity.this).inflate(
+                        R.layout.testor_item_layout, null);
+                holder = new ViewHolder();
+                holder.name = (TextView) view.findViewById(R.id.name);
+                holder.idCard = (TextView) view.findViewById(R.id.idcard);
+                holder.note = (TextView) view.findViewById(R.id.note);
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+            SurveyerInformation info = mTestors.get(pos);
+            holder.name.setText(info.getSurveyerName());
+            holder.idCard.setText(info.getCertificateID());
+            holder.note.setText(info.getInfo());
+            return view;
+        }
+
+    }
+
+    class ViewHolder {
+        TextView name;
+        TextView idCard;
+        TextView note;
+    }
 }
