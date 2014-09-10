b32169df9e720512d57f6e692daa4943cad6da37
ya0xu <287417257@qq.com>
2014-09-10 15:13:00 +0800

    非正常退出后，把原文件删除后，可能导致最后的更新没有写入到数据库问题

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/utils/CrtbDbFileUtils.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/utils/CrtbDbFileUtils.java
@@ -489,7 +489,11 @@ public final class CrtbDbFileUtils {
 			
 			// 7. 删除临时文件
 			File f = new File(tempPath);
-			if(f != null && f.exists())f.delete() ;
+			//YX 非正常退出后，把原文件删除后，可能导致最后的更新没有写入到数据库问题
+			if(f != null && f.exists()){
+			    //f.delete() ;
+				return new String[]{name,tempPath};
+			}
 			
 			// 解密
 			IDbEncrypt encrypt 	= new DbAESEncrypt() ;
