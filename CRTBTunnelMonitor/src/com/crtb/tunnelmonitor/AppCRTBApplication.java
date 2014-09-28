bbc0332837ad3dd95768c2fcadd71fb2ebe6524d
ya0xu <287417257@qq.com>
2014-09-28 10:37:22 +0800

    FixBug：整合周师兄的代码的Commit中，没有把他的测试代码（默认返回为已注册用户）注释掉

--- CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/AppCRTBApplication.java
+++ CRTBTunnelMonitor/src/com/crtb/tunnelmonitor/AppCRTBApplication.java
@@ -160,7 +160,7 @@ public class AppCRTBApplication extends Application {
     }
 
     public int getCurUserType() {
-        return CrtbUser.LICENSE_TYPE_REGISTERED;
+        return mCurUserType;
     }
 
     private void setCurUserType(int curUserType) {
